07e164dd4ccc33acb5c2d712d05a77e9d52a8f1c
Miryam Bathilde <mbathilde@gmail.com>
2021-03-09 18:14:25 +0100

    dando dinamismo a la portada post

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      // eslint-disable-next-line no-native-reassign
+      username:String="Cindy Bake", 
+      // eslint-disable-next-line no-native-reassign
+      caption:String="Today I'm feeling so mindfullness..." , 
+      // eslint-disable-next-line no-native-reassign
+      imageUrl:URL="https://images.unsplash.com/photo-1499728603263-13726abce5fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      // eslint-disable-next-line no-native-reassign
+      username:String="Travis Howard", 
+      // eslint-disable-next-line no-native-reassign
+      caption:String="Beginning the day with a cup of coffee." , 
+      // eslint-disable-next-line no-native-reassign
+      imageUrl:URL="https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=731&q=80",
+    }
+  ]);
+ 
   return (
     <div className="App">
       {/* Header */}
@@ -15,8 +35,16 @@ function App() {
 
       <h1>I'm building an Instagram Clone with React</h1>
 
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+
       {/* Posts */}
-      <Post />
+      <Post username="Cindy Bake" caption="Today I'm feeling so mindfullness..." imageUrl="https://images.unsplash.com/photo-1499728603263-13726abce5fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"  />
+      <Post username="Travis Howard" caption="Beginning the day with a cup of coffee." imageUrl="https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"  />
+      <Post username="Remy Sharp" caption="Doing my yoga meditation" imageUrl="https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=731&q=80"  />
       
       {/* Posts */}
 
--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Post.css';
+import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username, caption, imageUrl}) {
   return (
-    <div className="post">
-      <h3>Username</h3>
 
-      {/* header -> avatar + username */}
-      
-      
+    <div className="post">
+        {/* header -> avatar + username */}
+        <div className="post_header">
+            <Avatar 
+                className="post_avatar"
+                alt="Cindy Bake"
+                src="/static/images/avatar/3.jpg" 
+            />
+            <h3>{username}</h3>
+        </div>
+
       {/* image */}
-      <img className="post_image" src="https://images.unsplash.com/photo-1499728603263-13726abce5fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="/"></img>
+      <img className="post_image" src={imageUrl} alt="/"/>
       {/* username +  caption */}
-      <h4 className="post_text"><strong>Patricia:</strong> Today I'm feeling so mindfullness...</h4>
+      <h4 className="post_text"><strong>{username}:</strong> {caption}</h4>
     </div>
   );
 }
